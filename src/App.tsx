3ae5aa157ba25ef44942c5e04968f60c656e584d
Daniel Kim <daniel.k.0710@gmail.com>
2024-07-15 11:55:55 -0400

    Hide unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Lobby from './pages/Lobby';
-import Settings from './pages/Settings';
+// import Settings from './pages/Settings';
 import Layout from "./components/Layout";
 import Room from "./pages/Room";
-import Library from "./pages/Library";
-import About from "./pages/About";
+// import Library from "./pages/Library";
+// import About from "./pages/About";
 import './App.css';
 
 const App = () => {
@@ -15,11 +15,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login/>} />
           <Route element={<Layout/>}>
-            <Route path="/about" element={<About/>} />
+            {/* <Route path="/about" element={<About/>} /> */}
             <Route path="/lobby" element={<Lobby/>} />
-            <Route path="/library" element={<Library/>} />
+            {/* <Route path="/library" element={<Library/>} /> */}
             <Route path="/room" element={<Room />} />
-            <Route path="/settings" element={<Settings/>} />
+            {/* <Route path="/settings" element={<Settings/>} /> */}
           </Route>
         </Routes>
       </BrowserRouter>
